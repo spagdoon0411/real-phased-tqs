509f0b9c062a1a1924791755979f1391f863288f
spagdoon0411 <spandan.suthar@live.com>
2026-04-16 22:33:59 -0700

    add sampling routine

--- model/tqs.py
+++ model/tqs.py
@@ -4,6 +4,8 @@ from hamiltonian.hamiltonian import Hamiltonian
 import torch
 from torch import nn
 
+from torch.nn.functional import log_softmax
+
 
 class SinusoidalPositionalEncoding(nn.Module):
     """
@@ -47,6 +49,29 @@ class Embedding(nn.Module):
         return self.linear(x)
 
 
+class Deembedding(nn.Module):
+    """
+    Performs a linear map to obtain logits corresponding to next-token
+    probability distributions, then provides probability and possibly phase values.
+    """
+
+    def __init__(self, d_model: int, possible_values: int, device: torch.device):
+        super().__init__()
+        self.linear = nn.Linear(d_model, possible_values, device=device)
+        self.possible_values = possible_values
+
+    def forward(self, x: torch.Tensor, compute_phases: bool = False) -> torch.Tensor:
+        logits = self.linear(x)  # (seq_len, batch_size, possible_values)
+
+        log_probs = log_softmax(logits, dim=-1)
+
+        if compute_phases:
+            phases = self._softsign(logits)
+            return log_probs, phases
+
+        return log_probs
+
+
 class TransformerQuantumState(nn.Module):
     def __init__(
         self,
@@ -88,6 +113,7 @@ class TransformerQuantumState(nn.Module):
             ),
             num_layers=n_layers,
         )
+        self.deembedding = Deembedding(d_model, possible_values=self.spin_dim, device=device)
 
         self.register_buffer(
             "mask",
@@ -129,6 +155,13 @@ class TransformerQuantumState(nn.Module):
             spin_buffer[:, :, self.prefix_dim : self.prefix_dim + self.spin_dim] = spin_tokens
         return spin_buffer
 
+    @staticmethod
+    def _softsign(x: torch.Tensor) -> torch.Tensor:
+        """
+        Definition borrowed from Zhang and Ventra (see yuanhangzhang98/transformer_quantum_state).
+        """
+        return 2 * torch.pi * (1 + x / (1 + x.abs()))
+
     def forward(
         self,
         buffer: torch.Tensor,
@@ -142,4 +175,44 @@ class TransformerQuantumState(nn.Module):
         buffer = self.embedding(buffer)  # (seq_len, batch_size, d_model)
         buffer = self.pos_encoding(buffer)
         buffer = self.transformer(buffer, mask=self.mask, is_causal=True)
+        res = self.deembedding(buffer, compute_phases=compute_phases)
+        return res  # Either log_probs or (log_probs, phases)
+
+    def sample_states(self, buffer: torch.Tensor, start_at: int = 0) -> torch.Tensor:
+        """
+        Constructs states autoregressively, filling in the buffer provided.
+
+        NOTE: assumes the spins present in the buffer are unique---otherwise, sampling
+        and downstream computations are outright incorrect.
+        """
+
+        target_len = buffer.shape[0] - self.prefix_dim
+
+        for i in range(start_at, target_len):
+            # NOTE: values downstream of log_probs but upstream of random sampling are not relevant to the energy loss.
+            # See (Gradient Estimation Using Stochastic Computation Graphs, Schulman et al. 2016). Detaching frees
+            # memory that would have been used for adjoints.
+
+            target_idx = i + self.prefix_dim  # Index of next token to be sampled
+            log_probs = self.forward(buffer[:target_idx], compute_phases=False)
+            probs = log_probs.detach()[-1, :, :].exp()  # log_softmax handles normalization
+            sampled_idx = torch.multinomial(probs, num_samples=1).squeeze(-1)
+            buffer[target_idx, torch.arange(self.batch_size), self.prefix_dim + sampled_idx] = 1
+
+            # TODO: fit more chains and reduce redundant computations by forcing params and system dimensions
+            # to be constant across the batch dimension. Add a mode to do this.
+
         return buffer
+
+    def construct_wavefunction(self, log_probs: torch.Tensor, phases: torch.Tensor) -> torch.Tensor:
+        """
+        Produces site-wide contributions to the wavefunction from log-probs. Sum over the sequence dimension to obtain
+        one probability-amplitude value per batch entry (corresponding to a single basis state).
+
+        log_probs: natural log of the probability of sampling each value (spin) that was sampled.
+
+        phases: phase contributions to the probability amplitude of the final basis state, selected from the
+        "phase distribution" using the value actually sampled.
+        """
+
+        return torch.sqrt(log_probs.exp()) * torch.exp(1j * phases)
