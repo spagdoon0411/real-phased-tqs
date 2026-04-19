946402779549786e93deecdc3105afe46de8e860
spagdoon0411 <spandan.suthar@live.com>
2026-04-19 11:20:10 +0000

    add gradient clipping in training loop

--- training/training_loop.py
+++ training/training_loop.py
@@ -72,6 +72,7 @@ def train(
         optimizer.zero_grad(set_to_none=True)
         loss, _, _ = compute_grad(model, samples, sample_weight, Eloc)
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
 
         if on_step is not None:
